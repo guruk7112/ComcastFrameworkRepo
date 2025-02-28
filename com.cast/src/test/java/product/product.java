10d848be1a96dda681fa5b83ee7c9a3dd55cd72f
Gurup <Gurup@GURUKIRAN>
2025-02-28 14:55:58 +0530

    added element search

---
+++ com.cast/src/test/java/product/product.java
@@ -0,0 +1,13 @@
+package product;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class product {
+
+	
+	@FindBy(name="search")
+	private WebElement search;
+		
+	
+}
